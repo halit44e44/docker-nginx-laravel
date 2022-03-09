00f7ef765ec119383d345c58efea2aab757acca6
halit44e44 <halit.dogan@tsoft.com.tr>
2022-03-09 14:36:23 +0300

    ngix laravel

---
+++ .docker/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
+composer install
+
+#Just for Laravel Apps
+#php artisan migrate --seed
+
+php-fpm
